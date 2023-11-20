941742b819ae5b3b74dc9c1bf41d7a7e744417a9
Ankur Gupta <ankurnecessary@gmail.com>
2023-11-20 22:49:53 +1030

    Lecture 7: Unit testing with jasmine - Introduction to jasmine spies

--- 1_section_1_course_kick_off_lecture_3/src/app/courses/services/calculator.service.spec.ts
+++ 1_section_1_course_kick_off_lecture_3/src/app/courses/services/calculator.service.spec.ts
@@ -1,12 +1,19 @@
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
+/**
+ * When we test a service then we provide the real instance of that service only while writing it's test.
+ * For services which are injected we create a fake implementation.
+ */
 describe("CalculatorService", () => {
 
   it("should add 2 numbers", () => {
-    const calculator = new CalculatorService(new LoggerService());
+    const logger = jasmine.createSpyObj("LoggerService", ['log']); // create fake implementation of both LoggerService and it's log method
+    // spyOn(logger, "log"); // Creates a fake implementation of logger.log() method
+    const calculator = new CalculatorService(logger);
     const result = calculator.add(2, 2);
     expect(result).toBe(4);
+    expect(logger.log).toHaveBeenCalledTimes(1);
   })
 
   it("should subtract 2 numbers", () => {
