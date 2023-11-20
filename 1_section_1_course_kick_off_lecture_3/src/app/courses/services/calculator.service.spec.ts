faf3b6d83b86021eaf91a88851f768c6a2b5664e
Ankur Gupta <ankurnecessary@gmail.com>
2023-11-21 10:20:55 +1030

    Lecture 8: Structuring Angular Unit Tests - Test Setup using beforeEach

--- 1_section_1_course_kick_off_lecture_3/src/app/courses/services/calculator.service.spec.ts
+++ 1_section_1_course_kick_off_lecture_3/src/app/courses/services/calculator.service.spec.ts
@@ -5,23 +5,31 @@ import { LoggerService } from "./logger.service";
  * When we test a service then we provide the real instance of that service only while writing it's test.
  * For services which are injected we create a fake implementation.
  */
+
+// https://app.clickup.com/t/86ctzaqdd
 describe("CalculatorService", () => {
 
+  let loggerSpy: any,
+    calculator: CalculatorService;
+
+  beforeEach(() => {
+    console.log("Before each test");
+    loggerSpy = jasmine.createSpyObj("LoggerService", ['log']); // create fake implementation of both LoggerService and it's log method
+    calculator = new CalculatorService(loggerSpy);
+  })
+
   it("should add 2 numbers", () => {
-    const logger = jasmine.createSpyObj("LoggerService", ['log']); // create fake implementation of both LoggerService and it's log method
-    // spyOn(logger, "log"); // Creates a fake implementation of logger.log() method
-    const calculator = new CalculatorService(logger);
+    console.log("Add 2 numbers");
     const result = calculator.add(2, 2);
     expect(result).toBe(4);
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   })
 
   it("should subtract 2 numbers", () => {
-    const logger = jasmine.createSpyObj("LoggerService", ['log']);
-    const calculator = new CalculatorService(logger);
+    console.log("Subtract 2 numbers");
     const result = calculator.subtract(10, 5);
     expect(result).toBe(5, "unexpected subtraction result");
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   })
 
 })
