440edb46b906915fd7bdf9f812c9bf9a2de17fcd
Ankur Gupta <ankurnecessary@gmail.com>
2023-11-20 22:26:17 +1030

    Lecture 6: Step by step implementation of our first Jasmine Specification

---
+++ 1_section_1_course_kick_off_lecture_3/src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,18 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
+describe("CalculatorService", () => {
+
+  it("should add 2 numbers", () => {
+    const calculator = new CalculatorService(new LoggerService());
+    const result = calculator.add(2, 2);
+    expect(result).toBe(4);
+  })
+
+  it("should subtract 2 numbers", () => {
+    const calculator = new CalculatorService(new LoggerService());
+    const result = calculator.subtract(10, 5);
+    expect(result).toBe(5, "unexpected subtraction result");
+  })
+
+})
