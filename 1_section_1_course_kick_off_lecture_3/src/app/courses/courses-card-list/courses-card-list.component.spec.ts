b6d76cf39ab93dea2ba59d821f73a804fb6fab6f
Ankur Gupta <ankurnecessary@gmail.com>
2023-12-03 07:23:57 +1030

    Lecture 21: Angular component testing - DOM Interaction

--- 1_section_1_course_kick_off_lecture_3/src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ 1_section_1_course_kick_off_lecture_3/src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -15,17 +15,19 @@ describe('CoursesCardListComponent', () => {
 
   let component: CoursesCardListComponent;
   let fixture: ComponentFixture<CoursesCardListComponent>;
+  let el: DebugElement; // To query DOM
 
   beforeEach(waitForAsync(() => {
 
     // Setting testing environment
     TestBed.configureTestingModule({
-      imports: [CoursesModule]
+      imports: [CoursesModule] // To get all the components used in <CoursesCardListComponent>
     })
       .compileComponents()
       .then(() => {
         fixture = TestBed.createComponent(CoursesCardListComponent);
         component = fixture.componentInstance;
+        el = fixture.debugElement; // To query DOM
       });
 
   }));
@@ -39,7 +41,20 @@ describe('CoursesCardListComponent', () => {
 
   it("should display the course list", () => {
 
-    pending();
+    /*
+    1. Some DOM interaction is needed to confirm if the list appears on the screen or not
+    2. Going to fire change detection mechanism of angular manually to make sure that view reflects the latest data
+    3. We will also learn how to debug such a component test
+    */
+
+    // pass some data to the component to show
+    component.courses = setupCourses();
+
+    // Next we will make sure that data is getting displayed in the component by querying and asserting that the data is there
+    const cards = el.queryAll(By.css('.course-card'));// Querying DOM to find if the .course-card exists or not
+
+    expect(cards).toBeTruthy('Could not find cards');
+    expect(cards.length).toBe(12, "Unexpected number of courses"); // This will fail
 
   });
 
@@ -52,5 +67,3 @@ describe('CoursesCardListComponent', () => {
 
 
 });
-
-
